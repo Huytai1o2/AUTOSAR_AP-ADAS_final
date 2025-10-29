f6ba43b34e36c34bdea232ae14148b156859d344
Huytai1o2 <tai.nguyenraphael@hcmut.edu.vn>
2025-10-29 19:38:04 +0700

    upload file setup and project

---
+++ workspace/HPC_Machine/build/Actuator/src/CMakeFiles/Actuator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Actuator"
+  "Actuator.pdb"
+  "CMakeFiles/Actuator.dir/actuator/aa/actuator.cpp.o"
+  "CMakeFiles/Actuator.dir/actuator/aa/actuator.cpp.o.d"
+  "CMakeFiles/Actuator.dir/actuator/aa/port/sensor.cpp.o"
+  "CMakeFiles/Actuator.dir/actuator/aa/port/sensor.cpp.o.d"
+  "CMakeFiles/Actuator.dir/main.cpp.o"
+  "CMakeFiles/Actuator.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Actuator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
